d94cedab42174b62ff856d210e42d34249c9f058
Fabio Bertagna <bertagna@puzzle.ch>
2021-03-27 20:12:43 +0100

    Add OSTicket

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+mkdir dokuwiki_data
+chmod o+w dokuwiki_data
+mkdir -p osticket/data
+chmod o+w dokuwiki_data
+mkdir osticket/logs
+chmod o+w osticket/logs
+mkdir osticket_db
+chmod o+w osticket_db
